ac8bbd838f8de09f3b170aa3ffddde4c870de73a
j redd <jlredd@mtu.edu>
2026-03-23 00:31:58 -0400

    Read in the data into an unformatted char array

--- day1-Secret Entrance/solution.c
+++ day1-Secret Entrance/solution.c
@@ -1,5 +1,26 @@
 //gcc -o solution solution.c -g -Wall -pedantic
 
-int main() {
-	return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[]) {
+	// Open the file to read
+	int fd = open(argv[1], O_RDONLY);
+
+	off_t fileSize = lseek(fd, 0, SEEK_END);
+	lseek(fd, 0, SEEK_SET);
+
+	char *unformattedInstructions = malloc(fileSize + 1);
+
+	read(fd, unformattedInstructions, fileSize);
+	unformattedInstructions[fileSize] = 0;
+
+	close(fd);
+
+
+
+	int position = 50;
 }
