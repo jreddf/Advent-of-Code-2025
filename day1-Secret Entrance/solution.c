ed53ff64ffc27aef63021b3cfe10914736503d7f
j redd <jlredd@mtu.edu>
2026-03-23 10:46:27 -0400

    Part 1 of Day 1

--- day1-Secret Entrance/solution.c
+++ day1-Secret Entrance/solution.c
@@ -20,7 +20,69 @@ int main(int argc, char *argv[]) {
 
 	close(fd);
 
+	if (argc > 2) {
+		printf("Unformatted instructions:\n%s\n\n", unformattedInstructions);
+	}
+
+	// Read how many instructions
+	int numInstructions = 1; 
+	for (int i = 0; i < fileSize; i++) {
+		if (unformattedInstructions[i] == '\n') { numInstructions++; }
+	}
+
+	short instructions[numInstructions];
+	int instructionCounter = 0;
+	int startPoint = 0;
+
+	for (int i = 0; i <= fileSize; i++) {
+		if (unformattedInstructions[i] == '\n' || unformattedInstructions[i] == 0) {
+			int size = i - startPoint + 1;
+			char buf[size];
+
+			memcpy(buf, &unformattedInstructions[startPoint], i-startPoint);
+			buf[i-startPoint] = 0;
+
+			int dir = buf[0] == 'L' ? -1 : 1;
+			int val = atoi(&buf[1]);
+			instructions[instructionCounter++] = (dir * val) % 100;
+
+			startPoint = i+1;
+			i++;
+		}
+		
+	}
+
+	free(unformattedInstructions);
+
+	if (argc > 2) {
+		printf("numInstructions: %d\n", numInstructions);
+
+		printf("Formatted Instructions:\n");
+
+		for (int i = 0; i < numInstructions; i++) {
+			printf("%3d\n", instructions[i]);
+		}
+		printf("\n");
+	}
 
 
+	// Perform the instructions
 	int position = 50;
+	long zeroCount = 0;
+
+	if (argc > 2) {
+		printf("Position of the dial: starting at %d\n", position);
+	}
+
+	for (int i = 0; i < numInstructions; i++) {
+		position += instructions[i];
+		position = position < 0 ? position + 100 : position % 100;
+		if (position == 0) { zeroCount++; }
+
+		if (argc > 2) {
+			printf("After instruction %d: %3d: Zero count is %ld\n", i+1, position, zeroCount);
+		}
+	}
+
+	printf("Zero count : %ld\n", zeroCount);
 }
